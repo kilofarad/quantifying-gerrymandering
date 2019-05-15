bbf07b3de8dc5d84110979af57c705f691f377e9
Katrina <klf2133@columbia.edu>
2019-05-15 14:53:01 -0400

    Moving district code to load_graph.py

--- load_graph.py
+++ load_graph.py
@@ -10,6 +10,7 @@ def helper_flatten(iterable):
 
 
 def load_raw_data(pa_data, shp_file):
+    """Load raw data on Pennsylvania precincts from Duke data and shp file"""
     # load adjacency
     neighbors = {}
     with open(pjoin(pa_data, "NEIGHBOR_LIST.txt")) as f:
@@ -65,6 +66,8 @@ def load_graph(pa_data, shp_file):
     areas, populations, neighbors, edge_lengths, geoids, geometries = load_raw_data(
         pa_data, shp_file
     )
+    """
+    """
     # create node attribute dicts
     fids = []
     dictys = []
@@ -90,8 +93,65 @@ def load_graph(pa_data, shp_file):
             G.add_edge(fid, n, length=border_length)
     return G
 
+def load_districts(G, path_to_shp, node_attr_name, min_overlap=0.9):
+    """
+    Loads a shapefile of districts and assigns a district label to each precinct node in G
+    :param G: networkx Graph with precinct nodes. Precinct nodes must have a 'geometry' attribute
+        containing a shapely Polygon
+    :param path_to_shp: Path to shp file of a districting plan
+    :param node_attr_name: Name for district attribute in Graph node
+    :param min_overlap: Minimum fraction of precinct Polygon that intersects with district polygon
+        to consider contained within (no warning or count will be provided)
+    """
+    # load shapefile into dict
+    with fiona.open(path_to_shp) as f:
+        districts = {
+            i: Polygon(helper_flatten(row["geometry"]["coordinates"]))
+            for i, row in enumerate(f, start=1)
+        }
+
+    # map each district to a node
+    for node in G.nodes:
+        p_poly = G.nodes[node]["geometry"]
+        districted = False
+        d_cands = {}
+        for district, d_poly in districts.items():
+            # is precinct contained within district polygon?
+            if p_poly.within(d_poly):
+                G.nodes[node][node_attr_name] = district
+                districted = True
+                break
+            # does precinct intersect district polygon?
+            if min_overlap and p_poly.intersects(d_poly):
+                area = p_poly.intersection(d_poly).area
+                d_cands[district] = area
+                # if more than 90% is contained within a district, let's call it rounding error
+                if (area / p_poly.area) > min_overlap:
+                    G.nodes[node][node_attr_name] = district
+                    districted = True
+                    break
+
+        # if we have not mapped this precinct to a district
+        if not districted:
+            if len(d_cands) == 1:
+                # this doesn't occur when the 90% rule is added
+                G.nodes[node][node_attr_name] = list(d_cands.keys())[0]
+            elif len(d_cands) > 1:
+                print(f"Precinct with fid {node} is split between {n} districts:")
+                G.nodes[node][node_attr_name] = max(d_cands.keys(), key=d_cands.get)
+                for area in d_cands.values():
+                    print(f"{area/p_poly.area*100}%")
+            else:
+                print(f"{node} is not contained within any district!")
+    return districts
+
+
 
 path_to_pa = "../PennsylvaniaRedistrictingData/ExtractedData"
 path_to_shp = "../2011 Voting District Boundary Shapefiles/VTDS.shp"
+path_to_initial_districts = (
+    "../PA-Congressional-Districts-2011/BlockLevelFinalCongressionalPlan21Dec2011.shp"
+)
 
 G = load_graph(path_to_pa, path_to_shp)
+districts = load_districts(G, path_to_initial_districts, "district")
